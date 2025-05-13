6a67913eefb4e81999fcc0a5aa7ba0525d87e445
HarshK200 <harshkapadia04@gmail.com>
2025-05-14 01:03:50 +0530

    that sweet .clang-format and got a black window now! Still need to setup GLAD

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
 #include "GLFW/glfw3.h"
+#include <GL/gl.h>
+#include <iostream>
 
 int main() {
-    std::cout << "Hello GLFW + OpenGL" << std::endl;
+    if(!glfwInit()) {
+        std::cerr << "Couldn't Initiaize GLFW" << std::endl;
+        return -1;
+    }
 
+    std::cout << "Hello GLFW + OpenGL" << std::endl;
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 720, "Ballz", nullptr, nullptr);
+
+    if(!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // making window context current tells openGL that this window is to issue cammand to
+    glfwMakeContextCurrent(window);
+
+    // main loop
+    while(!glfwWindowShouldClose(window)) {
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    // NOTE: doesn't  this glfwDestroyWindow() call is not really need since
+    // glfwTerminate() destorys all window either way
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
