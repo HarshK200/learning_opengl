646361fe073e19cd0dca82bc4f83b45c7fb8d8ca
HarshK200 <harshkapadia04@gmail.com>
2025-05-14 18:08:00 +0530

    complied and ran the most basic vertex and fragment shader

--- application/src/Application.cpp
+++ application/src/Application.cpp
@@ -5,6 +5,20 @@
 #include <iostream>
 // clang-format on
 
+const char* vertexShaderCode = "#version 460 compatibility\n"
+                               "layout (location = 0) in vec3 aPos;\n"
+                               "void main()\n"
+                               "{\n"
+                               "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                               "}\0";
+
+const char* fragmentShaderCode = "#version 460 compatibility\n"
+                                 "out vec4 FragColor;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                 "}\0";
+
 // changes OpenGL viewport on resize
 void handleWindowResizeOpenGL(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -26,12 +40,16 @@ int main() {
 
     // always makes the window floating for i3wm
     // glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
     window = glfwCreateWindow(800, 600, "Ballz", nullptr, nullptr);
 
     if(!window) {
         glfwTerminate();
+        std::cout << "Couldn't create window" << std::endl;
         return -1;
     }
 
@@ -50,7 +68,7 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, handleWindowResizeOpenGL);
 
-    // NOTE: modern OpenGL traingle drawing here
+    // NOTE: modern OpenGL traingle drawing data here
 
     // clang-format off
     float VertexData[] = {
@@ -71,6 +89,49 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // compiling shaders
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Error compiling Vertex Shader:" << infoLog << std::endl;
+        return -1;
+    }
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "Error compiling Fragment Shader:" << infoLog << std::endl;
+        return -1;
+    }
+
+    // linking shaders
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Error LINKING Shader Programm:" << infoLog << std::endl;
+        return -1;
+    }
+
+    // setting/activating the shader program
+    glUseProgram(shaderProgram);
+
     // main loop
     while(!glfwWindowShouldClose(window)) {
         // handleing input events
@@ -84,7 +145,6 @@ int main() {
 
         /* RENDER HERE */
         glDrawArrays(GL_TRIANGLES, 0, 3);
-
 
         // poll for any new event
         glfwPollEvents();
